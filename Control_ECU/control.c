95b261550b37239362ed062f617f29face34efb4
Kareeem <kareem.asu11@gmail.com>
2021-10-29 23:15:46 +0200

    deinit function in the timer deriver

---
+++ Control_ECU/control.c
@@ -0,0 +1,14 @@
+/*
+ * control.c
+ *
+ *  Created on: 29 Oct 2021
+ *      Author: hp
+ */
+
+int main(void){
+	while(1){
+
+	}
+	return 0;
+}
+
---
+++ HMI_ECU/hmi.c
@@ -0,0 +1,14 @@
+/*
+ * hmi.c
+ *
+ *  Created on: 29 Oct 2021
+ *      Author: hp
+ */
+
+
+int main(void){
+	while(1){
+
+	}
+	return 0;
+}
