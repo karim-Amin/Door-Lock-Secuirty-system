c1c067d1972a9a4073f854cc1ea7f33cb31b7ff3
Kareeem <kareem.asu11@gmail.com>
2021-10-29 14:44:15 +0200

    remove redundunt code

--- Control_ECU/Buzzer.c
+++ Control_ECU/Buzzer.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 /*****************************************************************************************
  *  module : External Buzzer
@@ -31,37 +30,3 @@ void BUZZER_OFF(void){
 	GPIO_writePin(BUZZER_PORT_ID,BUZZER_PIN_ID,LOGIC_LOW);
 }
 
-=======
-
-/*****************************************************************************************
- *  module : External Buzzer
- *  file name : Buzzer.c
- *  description : source file for external buzzer module
- *  Author :  Karim Mohamed Amin
- *****************************************************************************************/
-#include "gpio.h"
-#include "buzzer.h"
-/****************************************************************************************
- * 										Function Definitions
- ***************************************************************************************/
-/*
- * Description : make the pin connected to the buzzer output to be able to drive
- * buzzer on or off
- */
-void BUZZER_init(void){
-	GPIO_setupPinDirection(BUZZER_PORT_ID,BUZZER_PIN_ID,PIN_OUTPUT);
-}
-/*
- * Description : Turn on the buzzer by output high on its pin
- */
-void BUZZER_ON(void){
-	GPIO_writePin(BUZZER_PORT_ID,BUZZER_PIN_ID,LOGIC_HIGH);
-}
-/*
- * Description : Turn off the buzzer by output low on its pin
- */
-void BUZZER_OFF(void){
-	GPIO_writePin(BUZZER_PORT_ID,BUZZER_PIN_ID,LOGIC_LOW);
-}
-
->>>>>>> main program structure
--- Control_ECU/I2C.h
+++ Control_ECU/I2C.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 /*************************************************************************************************************
  *  module : I2C
@@ -49,55 +49,3 @@ uint8 TWI_readWithNACK(void);
 uint8 TWI_getStatus(void);
 
 #endif /* I2C_H_ */
-=======
-
-/*************************************************************************************************************
- *  module : I2C
- *  file name : I2C.h
- *  description : header file for I2C module in AVR microcontroller
- *  Author :  Karim Mohamed Amin
- *************************************************************************************************************/
-
-#ifndef I2C_H_
-#define I2C_H_
-#include"std_types.h"
-/************************************************************************************************************
- * 										Definitions
- ************************************************************************************************************/
-/* I2C Status Bits in the TWSR Register */
-#define TWI_START         0x08 /* start has been sent */
-#define TWI_REP_START     0x10 /* repeated start */
-#define TWI_MT_SLA_W_ACK  0x18 /* Master transmit ( slave address + Write request ) to slave + ACK received from slave. */
-#define TWI_MT_SLA_R_ACK  0x40 /* Master transmit ( slave address + Read request ) to slave + ACK received from slave. */
-#define TWI_MT_DATA_ACK   0x28 /* Master transmit data and ACK has been received from Slave. */
-#define TWI_MR_DATA_ACK   0x50 /* Master received data and send ACK to slave. */
-#define TWI_MR_DATA_NACK  0x58 /* Master received data but doesn't send ACK to slave. */
-#define TWI_STATUS_MASK   0XF8 /*use this mask to get the status of TWI bus from the register TWSR*/
-#define F_CPU 8000000UL /*this is the frequency of the target device */
-/************************************************************************************************************
- * 									Types Declaration
- ************************************************************************************************************/
-/*
- * Normal Mode 100 Kb/s
- * Fast Mode 400 Kb/s
- */
-typedef enum{
-	Normal_mode =100000UL,Fast_mode =400000UL
-}BitRateType;
-typedef struct{
-	BitRateType bit_rate_type;
-	uint8 adress;
-}ConfigType;
-/************************************************************************************************************
- * 									functions prototypes
- ************************************************************************************************************/
-void TWI_init(const ConfigType* config_struct_ptr);
-void TWI_start(void);
-void TWI_stop(void);
-void TWI_write(uint8 data);
-uint8 TWI_readWithACK(void);
-uint8 TWI_readWithNACK(void);
-uint8 TWI_getStatus(void);
-
-#endif /* I2C_H_ */
->>>>>>> main program structure
