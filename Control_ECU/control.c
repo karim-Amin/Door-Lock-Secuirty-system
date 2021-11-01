8fa0d18dc3bca993e408b5e988c1524d43720cfb
Kareeem <kareem.asu11@gmail.com>
2021-11-01 11:46:02 +0200

    comments are inserted in this file

--- Control_ECU/control.c
+++ Control_ECU/control.c
@@ -38,16 +38,23 @@ uint8 CONTROL_checkTwoPasswords(const uint8* a_first_password_ptr,const uint8* a
  *                             The entry point (main method)                  *
  *******************************************************************************/
 int main(void){
-	/*to hold the password taken from the user*/
+	/*to hold the first_password taken from the user*/
 	uint8 first_password_buff[NUM_OF_PASSWORD_DIGIT];
+	/*to hold the second_password taken from the user*/
 	uint8 second_password_buff[NUM_OF_PASSWORD_DIGIT];
+	/*setup the UART configuration*/
 	config_struct s_uart_config = {no_parity,eigth_bits,one_stop_bit,Asynch,CONTROL_ECU_BAUD_RATE};
+	/*setup the I2C configuration*/
 	ConfigType s_twi_config = {Normal_mode,CONTROL_ADDRESS};
+
+	/* 	calling the init functions for each driver */
 	UART_init(&s_uart_config);
 	TWI_init(&s_twi_config);
+	DcMotor_init();
 	/*set the I-bit to be able to use the timer driver*/
 	SREG |= (1<<7);
 	while(1){
+		/*check if the passwords sent by HMI_ECU are identical and send to it the status*/
 		if(CONTROL_setupFirstPassword(first_password_buff,second_password_buff) == SUCCESS){
 			CONTROL_sendStatus(CONTROL_PASSWORD_MATCH);
 		}else{
@@ -60,18 +67,36 @@ int main(void){
 /*******************************************************************************
  *                              Functions Definitions                           *
  *******************************************************************************/
+/*
+ * Description : this function is synchronised with the UART in human machine interface and
+ * it loops until it takes the entire password.
+ */
 void CONTROL_receivePasswordFromHMI(uint8* password_ptr){
+	/*counter for the loop*/
 	uint8 count;
+	/*waiting until the HMI_ECU to be ready to send the password*/
 	while(UART_recieveByte()!= HMI_ECU_READY);
+	/*telling the HMI_ECU that control ECU is ready to receive the password */
 	UART_sendByte(CONTROL_ECU_READY);
+	/*looping until the length of the password*/
 	for(count = 0;count<NUM_OF_PASSWORD_DIGIT;count++){
 		password_ptr[count] = UART_recieveByte();
 	}
 }
+/*
+ * Description : to create new password you have to enter it twice this function will check
+ * if this two password matching or not
+ */
 uint8 CONTROL_checkTwoPasswords(const uint8* a_first_password_ptr,const uint8* a_second_password_ptr){
+	/*
+	 * 1) i is the counter for the loop
+	 * 2) flag is used to know if they matching or not
+	 */
 	uint8 i,flag = 0;
+	/*compare each number in first password with each number in the other one*/
 	for(i = 0;i < NUM_OF_PASSWORD_DIGIT;i++){
 		if(a_first_password_ptr[i] != a_second_password_ptr[i]){
+			/*flag raised means they not matching*/
 			flag = 1;
 		}
 	}
@@ -81,15 +106,30 @@ uint8 CONTROL_checkTwoPasswords(const uint8* a_first_password_ptr,const uint8* a
 		return TRUE;
 	}
 }
+/*
+ * Description : set the entire password in EEPROM in specific locations ,and make constraints on
+ *  this function do not let it change in the password
+ */
 void CONTROL_savePasswordInEEPROM(const uint8* password_ptr){
 	uint8 counter;
 	for(counter = 0;counter < NUM_OF_PASSWORD_DIGIT;counter++){
+		/*
+		 * 1) PASSWORD_ADDRESS_IN_EEPROM is the base address
+		 * 2) counter will be used as an offset
+		 */
 		EEPROM_writeByte(PASSWORD_ADDRESS_IN_EEPROM + counter,password_ptr[counter]);
 	}
 }
+/*
+ * Description : this function receives two passwords for HMI_ECU and check them
+ * if they match ,it will save this password in the EEPROM.
+ */
 uint8 CONTROL_setupFirstPassword(uint8* a_first_password_ptr,uint8* a_second_password_ptr){
+	/*take the first password THROUGH the UART*/
 	CONTROL_receivePasswordFromHMI(a_first_password_ptr);
+	/*take the second password THROUGH the UART*/
 	CONTROL_receivePasswordFromHMI(a_second_password_ptr);
+	/*if the two passwords are identical ,then go save them in EEPROM*/
 	if(CONTROL_checkTwoPasswords(a_first_password_ptr,a_second_password_ptr)){
 		CONTROL_savePasswordInEEPROM(a_first_password_ptr);
 		return SUCCESS;
@@ -97,6 +137,9 @@ uint8 CONTROL_setupFirstPassword(uint8* a_first_password_ptr,uint8* a_second_pas
 		return ERROR;
 	}
 }
+/*
+ * Description : it will send the status (matching or not matching).
+ */
 void CONTROL_sendStatus(uint8 state){
 	UART_sendByte(CONTROL_ECU_READY);
 	while(UART_recieveByte()!= HMI_ECU_READY);
